334558a243bd3c1541467a4028ad2bbd8b7acf2b
Shawn McCool <shawn@heybigname.com>
2019-02-27 21:31:44 +0100

    Revert "remove query string from route matching"
    This reverts commit 0f842da187f7e249e6ce9ac75110909b93535f42.

--- spec/RouteMatcherSpec.php
+++ spec/RouteMatcherSpec.php
@@ -1,5 +1,6 @@
 <?php namespace spec\Monolith\WebRouting;
 
+use Monolith\Collections\Map;
 use Monolith\Http\Request;
 use Monolith\WebRouting\CompiledRoute;
 use Monolith\WebRouting\CompiledRoutes;
@@ -18,25 +19,6 @@ class RouteMatcherSpec extends ObjectBehavior
     function it_can_match_routes()
     {
         $_SERVER['REQUEST_URI'] = '/article/1';
-        $_SERVER['REQUEST_METHOD'] = 'stub';
-
-        $request = Request::fromGlobals();
-
-        $route = new CompiledRoute('stub', '/article/{id}', ControllerStub::class, 'index', new Middlewares);
-
-        $compiledRoutes = CompiledRoutes::list($route);
-
-        $matchedRoute = $this->match($request, $compiledRoutes);
-
-        $matchedRoute->httpMethod()->shouldBe('stub');
-        $matchedRoute->uri()->shouldBe($route->uri());
-        $matchedRoute->controllerClass()->shouldBe(ControllerStub::class);
-        $matchedRoute->controllerMethod()->shouldBe('index');
-    }
-
-    function it_ignores_query_strings_for_route_matching()
-    {
-        $_SERVER['REQUEST_URI'] = '/article/1?this=test';
         $_SERVER['REQUEST_METHOD'] = 'stub';
 
         $request = Request::fromGlobals();
